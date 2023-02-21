acfcb3b1c0140678632c88d293e13cca8230e794
FamaNiasse <famasimplon@gmail.com>
2023-02-21 20:02:17 +0100

    Création de la page modify-table, ajout des fonctions des boutons du header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+selector: 'app-header',
+templateUrl: './header.component.html',
+styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+constructor(private router: Router) { }
 
+redirectToModifyTable() {
+this.router.navigate(['/modify-table']);
+}
+
+redirectToCart() {
+this.router.navigate(['/cart']);
+}
 }
