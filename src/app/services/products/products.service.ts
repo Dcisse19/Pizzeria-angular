446648b6e10255396fb697f5cf3e5469b7f4c2e1
Diambere Cisse <diambere.cisse@outlook.com>
2023-02-22 14:33:09 +0100

    customize-page dynamique

--- src/app/services/products/products.service.ts
+++ src/app/services/products/products.service.ts
@@ -6,34 +6,25 @@ import { IProduct, IProductsByCategory, PRODUCTS } from 'src/app/mocks/products.
 })
 export class ProductsService {
 
-  // categories: IProductsByCategory[] = PRODUCTS;
-  product!: IProduct;
+ 
 
-  // products: IProduct = this.categories.products;
   constructor() { }
 
-  getProductsByCategory(categoryName: string): IProductsByCategory | undefined {
-    const foundCategory = PRODUCTS.find((p: IProductsByCategory) => p.category === categoryName);
-    return foundCategory;
-  }
+  // getProductsByCategory(categoryName: string): IProductsByCategory | undefined {
+  //   const foundCategory = PRODUCTS.find((p: IProductsByCategory) => p.category === categoryName);
+  //   return foundCategory;
+  // }
   getProductsByTag() { }
 
   createNewArray(): IProduct[] {
-    // const products: IProduct[] = [];
-
     const products = PRODUCTS[0].products.concat(PRODUCTS[1].products,PRODUCTS[2].products)
-    // PRODUCTS.forEach(category => {
-    //   products.concat(category.products);
-      console.log(products);
-    // })
     return products;
   }
 
   getProductById(id: number): IProduct | undefined {
     const products = this.createNewArray();
-    const foundProduct = products.find((product: IProduct) => product.id = id)
-    console.log('Produit trouvé : ', foundProduct);
-    return this.product;
+    const foundProduct = products.find((product: IProduct) => product.id === id)
+    return foundProduct;
   }
 }
 
