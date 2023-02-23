6976fc2fd275db8b42e2940d7881bcfd629c69d4
FamaNiasse <famasimplon@gmail.com>
2023-02-23 10:48:29 +0100

    resolution conflit

--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -1,9 +1,81 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { IProduct } from 'src/app/mocks/products.mock';
+import { ProductsService } from '../products/products.service';
 
+export interface CartProduct{
+  product: IProduct;
+  quantity: number;
+}
+
+export interface CartContent{
+  table : number;
+  cartProducts: CartProduct[];
+}
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  
+  table!:number;
+  totalPrice: number = 0;
+  productQuantity: number = 0;
+  constructor(
+    private productService: ProductsService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  // Création du panier
+  private createCart(){
+    const newCart : CartProduct[] = [];
+    const newCartString = JSON.stringify(newCart);
+    localStorage.setItem('cart', newCartString);
+  }
+
+  // Sauvegarde du panier
+  private saveCart(cart: CartProduct[]){
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
+
+  getCart(){
+    const cart = localStorage.getItem('cart');
+    if(cart){
+      return JSON.parse(cart);
+    } else {
+      this.createCart();
+      this.getCart();
+
+    }
+  }
+
+  initCart(){
+    this.getCart();
+    // this.getTotalQuantity();
+    // this.getTotalPrice();
+  }
+
+  resetCart(){
+    this.createCart();
+    // this.getTotalPrice();
+    // this.getTotalQuantity();
+  } 
+
+  addProductToCart(cartProduct: CartProduct){
+    const cart = this.getCart();
+    cart.push(cartProduct);
+    this.saveCart(cart);
+     // this.getTotalQuantity();
+    // this.getTotalPrice();
+    this.router.navigate(['/cart']);
+  }
+
+  removeProductFromCart (index:number){
+    const cart = this.getCart();
+    cart.splice(index,1);
+    this.saveCart(cart);
+    // this.getTotalQuantity();
+    // this.getTotalPrice();
+  }
+
+
 }
