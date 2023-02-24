23ce2198bd8737e7685c3f9d3b4d016634a6d563
your_github_username <liliana.guderska@gmail.com>
2023-02-24 14:19:52 +0100

    page products vide

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,31 +1,16 @@
 import { Component } from '@angular/core';
-import { IProductsByCategory, ITag, PRODUCTS, TAGS } from 'src/app/mocks/products.mock';
+import {
+  IProductsByCategory,
+  ITag,
+  PRODUCTS,
+  TAGS,
+} from 'src/app/mocks/products.mock';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
-  categories: IProductsByCategory[]= PRODUCTS;
-  tags: ITag[]= TAGS;
-
-  receivedTags: ITag[]=[];
-  index! :number;
   
-  setReceivedTags(receivedTag:ITag) {
-    if(receivedTag.isSelected){
-    this.receivedTags.push(receivedTag);
-    }
-   else {
-     const foundTag= this.receivedTags.find((tag)=>tag.id===receivedTag.id);
-     if (foundTag){
-      this.index=this.receivedTags.indexOf(foundTag);
-      this.receivedTags.splice(this.index,1);
-     }
-    }
-    console.log(this.receivedTags)
   }
-
-
-}
