dda98eb4d5deabc1ae07036b523ba7cc402208a9
FamaNiasse <famasimplon@gmail.com>
2023-02-25 18:26:16 +0100

    Les derniers bugs daffichage sont corrigé dernier push projet finalisé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Location } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { IProduct, PRODUCTS } from './mocks/products.mock';
+import { Router, NavigationEnd } from '@angular/router';
 import { CartService } from './services/cart/cart.service';
 import { TableServiceService } from './services/table-service.service';
-
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -15,16 +14,20 @@ export class AppComponent {
 
   hideFooter: boolean = true;
 
-  constructor(private cartService: CartService, private tableService: TableServiceService, private router : Router, private location: Location){}
+  constructor(private cartService: CartService, private tableService: TableServiceService, private router: Router, private location: Location) {}
 
-
-  ngOnInit(){
+  ngOnInit() {
     this.cartService.initCart();
-    this.tableService. getSelectedTable();
+    this.tableService.getSelectedTable();
     this.tableService.redirectIfNoTable();
 
-    const path = this.location.path();
-    const regex = /(modify-table|recapitulatif)/;
-    this.hideFooter = !(path === '' || regex.test(path));
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const path = window.location.pathname;
+        const regex = /(modify-table|recapitulatif)/;
+        this.hideFooter = !(path === '' || regex.test(path));
+      });
   }
+
 }
